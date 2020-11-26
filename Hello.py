3db8d85b75b542eaaf5b1d5cc8b9b7782440c6b2
buildertester <69243717+buildertester@users.noreply.github.com>
2020-11-26 12:11:07 +0900

    Update Hello.py
    Updated from script to function allowing multiple name input arguments. Description and use example provided in function body.

--- Hello.py
+++ Hello.py
@@ -1,1 +1,19 @@
-print('Hello buildertester')
+def Hello(*names):
+    """Function Concatenates 'Hello ' to the argument
+    
+    ----------
+    Arguments
+    names- must be a string datatype, multiple strings 
+    can be provided with comma separation
+    
+    ----------
+    Example
+        Hello('buildertester', 'Bob', 'Sarah')
+        
+    Output
+        Hello buildertester
+        Hello Bob
+        Hello Sarah"""
+    
+    for name in names:
+        print('Hello '+ name)
